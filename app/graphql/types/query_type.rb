4b48d66529782b87da5de14c1e947b4a04966623
EasonKaku <eason.kuo@wordup.com.tw>
2025-06-24 21:30:39 +0800

    modify query_type with courses and course

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,16 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+    field :courses, [Types::CourseType], null: false
+    def courses
+      Course.includes(sections: :units).all
     end
 
-    def node(id:)
-      context.schema.object_from_id(id, context)
+    field :course, Types::CourseType, null: true do
+      argument :id, ID, required: true
     end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def course(id:)
+      Course.includes(sections: :units).find_by(id: id)
     end
   end
 end
